5235f28ed8457497964254fdb54d57d767872d05
TURKCELL\TCABUYUKHATIPOGLU <arda.buyukhatipoglu@turkcell.com.tr>
2024-01-30 20:53:53 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by ARDA BUYUKHATIPOGLU on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
